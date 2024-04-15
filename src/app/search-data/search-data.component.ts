38b1c3e082d48d47d4b5326867bea41e6ed3a4d5
Alex-kod <alexrangel@live.fr>
2024-04-15 12:42:50 +0200

    tableau

--- src/app/search-data/search-data.component.ts
+++ src/app/search-data/search-data.component.ts
@@ -4,12 +4,40 @@ import { MatCardModule } from '@angular/material/card';
 import { FormsModule } from '@angular/forms';
 import { Component, ViewChild } from '@angular/core';
 import { MatAccordion, MatExpansionModule } from '@angular/material/expansion';
+import {
+  CdkDragDrop,
+  CdkDrag,
+  CdkDropList,
+  moveItemInArray,
+} from '@angular/cdk/drag-drop';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 import { provideNativeDateAdapter } from '@angular/material/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  { position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H' },
+  { position: 2, name: 'Helium', weight: 4.0026, symbol: 'He' },
+  { position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li' },
+  { position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be' },
+  { position: 5, name: 'Boron', weight: 10.811, symbol: 'B' },
+  { position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C' },
+  { position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N' },
+  { position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O' },
+  { position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F' },
+  { position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne' },
+];
+
 @Component({
   selector: 'app-search-data',
   standalone: true,
@@ -26,10 +54,28 @@ import { provideNativeDateAdapter } from '@angular/material/core';
     MatExpansionModule,
     MatIconModule,
     MatDatepickerModule,
+    MatTableModule,
+    CdkDropList,
+    CdkDrag,
   ],
   templateUrl: './search-data.component.html',
   styleUrl: './search-data.component.css',
 })
 export class SearchDataComponent {
   @ViewChild(MatAccordion) accordion!: MatAccordion;
+
+  columns: string[] = ['position', 'name', 'weight', 'symbol'];
+
+  dataSource = ELEMENT_DATA;
+
+  removeColumn(colonneASupprimer: string) {
+    let index = this.columns.indexOf(colonneASupprimer);
+    console.log('index colonne supprimé : ' + index);
+    this.columns.splice(index, 1);
+    console.log(this.columns);
+  }
+
+  drop(event: CdkDragDrop<string[]>) {
+    moveItemInArray(this.columns, event.previousIndex, event.currentIndex);
+  }
 }
